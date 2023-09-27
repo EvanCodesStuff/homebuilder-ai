aee03da1512fcb707dcf117fc7675d860db3781c
Evan K <evan@evankersey.com>
2023-09-27 09:54:14 -0500

    basic spring app

---
+++ src/main/java/com/example/homebuilder/HomebuilderApplication.java
@@ -0,0 +1,13 @@
+package com.example.homebuilder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomebuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomebuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/homebuilder/HomebuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.homebuilder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomebuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
