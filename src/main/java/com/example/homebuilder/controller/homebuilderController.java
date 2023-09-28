27b58511991c6d7920593a340dbb801659a85a39
Brandon Philachack <blp170230@utdallas.edu>
2023-09-28 11:47:07 -0500

    Added DawgFinding functionality

--- src/main/java/com/example/homebuilder/controller/homebuilderController.java
+++ src/main/java/com/example/homebuilder/controller/homebuilderController.java
@@ -2,6 +2,7 @@ package com.example.homebuilder.controller;
 
 import com.example.homebuilder.models.HomeInventory;
 import com.example.homebuilder.models.HomeRebuild;
+import com.example.homebuilder.models.DawgModel;
 import com.example.homebuilder.services.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -23,6 +24,9 @@ public class homebuilderController {
     @Autowired
     private HomeRebuildService homeRebuildService;
 
+    @Autowired
+    private DawgService dawgServicing;
+
     @GetMapping("/{HOME_ID}/inventory")
     public ResponseEntity<HomeInventory> getHomeInventory(@PathVariable("HOME_ID") int homeId) {
         HomeInventory homeInventory = homeInventoryService.getHomeInventory(homeId);
@@ -43,4 +47,13 @@ public class homebuilderController {
         }
     }
 
+    @GetMapping("/{FAMILY}/dogFinding")
+    public ResponseEntity<DawgModel> getDawg(@PathVariable("FAMILY") String family) {
+        DawgModel dawgFinder = dawgServicing.getDawgModel(family);
+        if (family != null) {
+            return ResponseEntity.ok(dawgFinder);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
