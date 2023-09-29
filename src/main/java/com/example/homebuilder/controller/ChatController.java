3f4465b57669cec56297ce4affb6d1a9257e2ed5
Evank <evan@evankersey.com>
2023-09-29 14:01:54 -0500

    Added functionality for Lonnie's endpoint

--- src/main/java/com/example/homebuilder/controller/ChatController.java
+++ src/main/java/com/example/homebuilder/controller/ChatController.java
@@ -26,23 +26,6 @@ public class ChatController {
     @Value("${openai.api.url}")
     private String apiUrl;
 
-    @GetMapping("/chat")//TODO step 3 create UI interface for chat bot
-    public String chat(@RequestParam String prompt) {
-        // create a request
-        System.out.println(prompt);
-        ChatRequest request = new ChatRequest(model, prompt,homeBuilderProps.getSystem());
-        request.setN(1);
-
-        ChatResponse response = restTemplate.postForObject(apiUrl, request, ChatResponse.class);
-
-        if (response == null || response.getChoices() == null || response.getChoices().isEmpty()) {
-            return "No response";
-        }
-
-        // return the first response
-        return response.getChoices().get(0).getMessage().getContent();
-    }
-
     private String buildChat(String prompt){
         // create a request
         System.out.println(prompt);
